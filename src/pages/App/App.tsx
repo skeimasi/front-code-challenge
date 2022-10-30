81f7ef45e1f7b7c1195c35dd3aac5d62b017029d
s.keimasi <skeimasi.devel@gmail.com>
2022-10-30 08:18:56 +0330

    added help dialog

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Box, Chip, Typography } from '@mui/material';
+import { Help } from '@mui/icons-material';
+import { AppBar, Box, Chip, IconButton, Modal, Typography } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ProductListView from '../../components/ProductListView';
@@ -8,6 +9,17 @@ import { removeFromList } from '../../reducers/listReducer';
 import { RootState } from '../../store/store';
 import './App.css';
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
 function App() {
 
   const selectedList = useSelector((store: RootState) => store.listSlice.items);
@@ -16,10 +28,15 @@ function App() {
   const UserListWithSearchBar = React.useMemo(() => WithSearchBar(UserListView), []);
   const ProductListWithSearchBar = React.useMemo(() => WithSearchBar(ProductListView), []);
 
+  const [help, showHelp] = React.useState(false);
+
   return (
     <div className="App">
-      <AppBar style={{ padding: 5 }}>
+      <AppBar style={{ padding: 5, display: "flex", justifyContent: "space-between", flexDirection: "row" }}>
         <Typography>Code Challenge</Typography>
+        <IconButton onClick={() => showHelp(true)}>
+          <Help style={{ color: "white" }} />
+        </IconButton>
       </AppBar>
 
       <div className='list'>
@@ -42,6 +59,19 @@ function App() {
           }
         </Box>
       </div>
+
+      <Modal open={help} onClose={() => showHelp(false)}>
+        <Box sx={style} dir='rtl'>
+          <Typography variant='h4'>آیتم های مورد نیاز</Typography>
+          <Typography>با استفاده از api  های فیک دو لیست دریافت کرده و در دو کامپوننت مجزا نمایش داده شوند</Typography>
+          <Typography>هر کدام از لیست ها قابلیت فیلتر داشته باشند</Typography>
+          <Typography>با کلیک بر روی آیتم های هر لیت ، آن آیتم به لیست دست راستی اضافه شود</Typography>
+          <Typography>با کلیک بر روی آیتم های لیست دست راست، آن آیتم حذف شود</Typography>
+          <Typography>از typescript , redux , material ui  استفاده شود</Typography>
+          <Typography></Typography>
+        </Box>
+      </Modal>
+
     </div>
   );
 }
